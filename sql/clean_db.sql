1d3a98cc171e4012dc42771503ca73f2971a2749
Grant <grant.williamson@utas.edu.au>
2018-08-06 11:10:24 +1000

    Added SQL clean database setup file

---
+++ sql/clean_db.sql
@@ -0,0 +1,47 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    user_id     uuid DEFAULT uuid_generate_v1(),
+    name       varchar(400) NOT NULL,
+    email       varchar(400) NOT NULL,
+    password   varchar(400) NOT NULL,
+    admin        boolean,
+    created_at   timestampz DEFAULT NOW(),
+	PRIMARY KEY (user_id)
+);
+
+CREATE TABLE roles (
+     role_id uuid DEFAULT uuid_generate_v1(),
+     user_id uuid,
+	 role varchar(50) NOT NULL,
+	 created_at timestampz DEFAULT NOW(),
+	 PRIMARY KEY (role_id)
+);
+
+CREATE TABLE datapacks (
+     datapack_id uuid DEFAULT uuid_generate_v1(),
+     user_id uuid,
+	 name varchar(400) NOT NULL,
+	 description text,
+	 data_year integer NOT NULL,
+	 uploaded_at timestampz DEFAULT NOW(),
+     dir_hash uuid,
+	 size integer,
+	 private boolean,
+	 PRIMARY KEY (datapack_id)
+);
+
+CREATE TABLE analysis (
+     analysis_id uuid DEFAULT uuid_generate_v1(),
+     user_id uuid,
+	 name varchar(400) NOT NULL,
+	 description text,
+	 run_year integer NOT NULL,
+	 created_at timestampz DEFAULT NOW(),
+	 completed_at timestampz,
+	 input_dir_hash uuid,
+     output_dir_hash uuid,
+	 status varchar(20),
+	 PRIMARY KEY (analysis_id)
+);
+
