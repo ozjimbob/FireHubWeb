ccff95eec36849931f575f0f6ff840892e448941
Grant <grant.williamson@utas.edu.au>
2021-02-04 09:55:10 +1100

    While adding servers to the kill/keep scripts to protect them, discovered a missing bracket which means this would never have run. Will watch it to make sure it behaves.

--- R/keep_servers.r
+++ R/keep_servers.r
@@ -10,7 +10,7 @@ args = commandArgs(trailingOnly=TRUE)
 # uuid name for the server
 server_names = args[1]
 server_names = strsplit(server_names,",")[[1]]
-server_names = c(server_names,"airrater-01","tiles")
+server_names = c(server_names,"airrater-01","tiles","NSWFireHub","firehub-web")
 
 print("OAuth")
 # Auth with DigitalOcean
@@ -27,7 +27,7 @@ print(tokill)
 
 if(length(tokill) > 0){
 for(i in tokill){
-  if(i %in% c("airrater-01","tiles"){
+  if(i %in% c("airrater-01","tiles","NSWFireHub","firehub-web")){
        next
   }
     dropnames = names(droplets())
@@ -35,7 +35,7 @@ for(i in tokill){
       print("Existing server for this analysis found - must be orphan. Deleting")
       id = which(dropnames == i)[1]
       to_delete = droplets()[[id]]
-      if(to_delete$name %in% c("airrater-01","tiles")){next}
+      if(to_delete$name %in% c("airrater-01","tiles","NSWFireHub","firehub-web")){next}
       try({droplet_delete(to_delete)})
       Sys.sleep(15)
     }
